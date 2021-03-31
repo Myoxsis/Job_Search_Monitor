7c5df339476fc3b12a540c057f616b068df14908
Maxime Alain <myoxsis@gmail.com>
2021-04-01 00:06:16 +0200

    Update April 2021

--- server/scrap.js
+++ server/scrap.js
@@ -5,49 +5,50 @@ const idx = require("./OfferModel");
 const ora = require('ora');
 
 
-async function scrapPSA() {
-    const page_url = 'https://jobs.groupe-psa.com/offre-de-emploi/liste-offres.aspx?lcid=1036&facet_JobDescription_Contract=577&mode=list';
+async function scrapTotal() {
+    const page_url = 'https://krb-sjobs.brassring.com/TGnewUI/Search/Home/Home?partnerid=30080&siteid=6559#home';
     const { data } = await axios.get(page_url);
     const $ = cheerio.load(data);
     var list_offers = [];
 
 
-    $('ul.ts-related-offers__row li').each((i, element) => {
+    $('li').each((i, element) => {
         const $element = $(element);
         const offers = {};
-        offers.name = $element.find($('a.ts-offer-list-item__title-link')).text().replace(/\s\s+/g, ' ').trim();
-        offers.link = ("https://jobs.groupe-psa.com" + $element.find($('a.ts-offer-list-item__title-link')).attr('href'));
-        offers.company = "PSA";
-        offers.function = "";
-        offers.details = $element.find('ul.ts-offer-list-item__description').find('li').map((i, el) => {
-            return $(el).text();
-          }).get().join(' /*/ ').replace(/\s\s+/g, ' ').trim();
+        //offers.name = $element.find($('a.jobtitle')).text().replace(/\s\s+/g, ' ').trim();
+        console.log($element);
+        //offers.link = ("https://jobs.groupe-psa.com" + $element.find($('a.ts-offer-list-item__title-link')).attr('href'));
+        //offers.company = "PSA";
+        //offers.function = "";
+        //offers.details = $element.find('ul.ts-offer-list-item__description').find('li').map((i, el) => {
+        //    return $(el).text();
+        //  }).get().join(' /*/ ').replace(/\s\s+/g, ' ').trim();
 
-        if (offers.name !== '') {
-            list_offers.push(offers);
-        }
+        //if (offers.name !== '') {
+        //    list_offers.push(offers);
+        //}
     });
-    const spinner = ora('Scrapping PSA\n');
-    spinner.start();
-    for (var i = 0; i < list_offers.length; i++) {
-        try{
-            console.log(list_offers[i].link);
-            const { data } = await axios.get(list_offers[i].link);
-            const $ = cheerio.load(data);
+    //const spinner = ora('Scrapping PSA\n');
+    //spinner.start();
+    //for (var i = 0; i < list_offers.length; i++) {
+    //    try{
+    //        console.log(list_offers[i].link);
+    //        const { data } = await axios.get(list_offers[i].link);
+    //        const $ = cheerio.load(data);
 
-            list_offers[i].desc = $('div#detail_offre').text().replace(/\s\s+/g, ' ').trim();
+    //        list_offers[i].desc = $('div#detail_offre').text().replace(/\s\s+/g, ' ').trim();
 
             //idx.add_to_db(list_offers[i]);
-        } catch(e) {
-            console.log(e.message);
-        }
-        console.log(list_offers[i]);
-    };
-    spinner.succeed();
+    //    } catch(e) {
+    //        console.log(e.message);
+    //    }
+    //    console.log(list_offers[i]);
+    //};
+    //spinner.succeed();
     //console.log("Scrapped Total : " + list_offers.length);
 };
 
-scrapPSA();
+scrapTotal();
 
 
 /*async function scrapSodern() {
